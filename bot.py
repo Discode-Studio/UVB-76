78df651ee64ada49162594b1596a61a26c7cf947
MirossOffYT <74865590+MirossOFFYT@users.noreply.github.com>
2024-08-25 20:21:23 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,47 +1,33 @@
 import discord
 from discord.ext import commands
 import subprocess
+import tempfile
 import os
 
-TOKEN = os.getenv("DISCORD_BOT_TOKEN")  # Utilisation du secret GitHub pour le token
-
-# Configurer le bot Discord
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user} est connecté à Discord!')
-    
-    # Connexion au salon vocal
-    voice_channel = discord.utils.get(bot.get_all_channels(), name='3')  # Remplace par le nom de ton salon vocal
-    if voice_channel is None:
-        print("Salon vocal non trouvé.")
-        return
-    
-    vc = await voice_channel.connect()
-    
-    # URL du flux audio WebSDR
-    stream_url = "http://websdr.78dx.ru:8901/"  # Remplace par l'URL correcte du flux audio si nécessaire
+    print(f'Logged in as {bot.user.name}')
+    voice_channel = discord.utils.get(bot.guilds[0].channels, name='3')
+    if voice_channel:
+        vc = await voice_channel.connect()
 
-    # Utiliser FFmpeg pour lire le flux audio dans Discord
-    process = subprocess.Popen(
-        ["ffmpeg", "-i", stream_url, "-f", "s16le", "-ar", "48000", "-ac", "2", "pipe:1"],
-        stdout=subprocess.PIPE
-    )
-    
-    # Jouer l'audio dans Discord
-    vc.play(discord.FFmpegPCMAudio(process.stdout))
-    
-    # Optionnel : ajouter un message pour confirmer que l'audio est joué
-    await voice_channel.send("Je joue maintenant le flux audio du WebSDR.")
+        url = 'http://websdr.78dx.ru:8901/'
 
-@bot.command()
-async def stop(ctx):
-    if ctx.voice_client:
-        await ctx.voice_client.disconnect()
-    else:
-        await ctx.send("Je ne suis pas connecté à un salon vocal.")
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.wav') as temp_file:
+            temp_file_path = temp_file.name
+            print(f'Temporary file created at: {temp_file_path}')
 
-bot.run(TOKEN)
+        command = [
+            'ffmpeg',
+            '-i', url,
+            '-f', 'wav',
+            temp_file_path
+        ]
+        subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        vc.play(discord.FFmpegPCMAudio(temp_file_path))
+
+# Le token est maintenant récupéré depuis une variable d'environnement
+bot.run(os.getenv('DISCORD_BOT_TOKEN'))
