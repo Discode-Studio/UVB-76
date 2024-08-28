19a3a3725c1b5babd1fe852fa4084bb32962816b
MirossOffYT <74865590+MirossOFFYT@users.noreply.github.com>
2024-08-28 13:50:25 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ async def on_ready():
     # Parcourir tous les serveurs auxquels le bot est connecté
     for guild in bot.guilds:
         # Vérifier si un salon vocal "General" existe
-        voice_channel = discord.utils.get(guild.voice_channels, name="General")
+        voice_channel = discord.utils.get(guild.voice_channels, name="UVB-76")
         
         if voice_channel:
             # Connecter le bot au salon vocal
@@ -38,7 +38,7 @@ async def on_ready():
             await play_uvb_stream(vc)
         else:
             # Créer le canal "General" s'il n'existe pas
-            voice_channel = await guild.create_voice_channel("General")
+            voice_channel = await guild.create_voice_channel("UVB-76")
             vc = await voice_channel.connect()
 
             # Diffuser UVB-76
@@ -48,11 +48,11 @@ async def on_ready():
     while True:
         await asyncio.sleep(10)
         for guild in bot.guilds:
-            voice_channel = discord.utils.get(guild.voice_channels, name="General")
+            voice_channel = discord.utils.get(guild.voice_channels, name="UVB-76")
 
             if not voice_channel:
                 # Si un canal vocal "General" n'existe pas, le créer
-                voice_channel = await guild.create_voice_channel("General")
+                voice_channel = await guild.create_voice_channel("UVB-76")
                 vc = await voice_channel.connect()
                 await play_uvb_stream(vc)
             else:
