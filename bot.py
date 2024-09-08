e7d9128703662eef6e745b48842f1aae64cfa4e4
MirossOffYT <74865590+MirossOFFYT@users.noreply.github.com>
2024-09-08 23:03:54 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Variables globales
-uvb_stream_url = 'https://github.com/Discode-Studio/AudioSource/raw/main/Ufopornoo!%20-%20Higher%20Quality%20(now%20with%20English%20subtitles).mp3'  # URL du stream UVB-76
+uvb_stream_url = 'https://github.com/Discode-Studio/AudioSource/raw/main/alien.wav'  # URL du stream UVB-76
 
 # Fonction pour jouer le stream UVB-76
 async def play_uvb_stream(vc):
